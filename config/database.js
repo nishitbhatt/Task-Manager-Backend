fb18b0844ad66d98bdd8321365c65848ba97f246
nishitbhatt <nishit.bhatt96@gmail.com>
2022-11-12 15:56:08 +0530

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ const db_connect = () => {
     // Connecting to the database
     const { MEDIUM, MONGO_URI, LIVE_MONGO_URI } = process.env;
 
-    // let DB_MONGO_URL = 'mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/zentodo'
-    let DB_MONGO_URL = 'mongodb://localhost:27017/zentodo-app'
+    let DB_MONGO_URL = 'mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/zentodo'
+    // let DB_MONGO_URL = 'mongodb://localhost:27017/zentodo-app'
 
     mongoose
         .connect(DB_MONGO_URL)
