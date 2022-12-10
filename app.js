2df19cc0813039aae39f974993aaed62d17cc9e4
nishitbhatt <nishit.bhatt96@gmail.com>
2022-12-10 17:49:09 +0530

    Cors

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ import { default as UserTasksRoutes } from './routes/user-tasks.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    console.log('HEREEEE')
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+//     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     console.log('HEREEEE')
+//     next();
+// });
 
 // User Routes
 app.use('/api/user', UserRoutes);
