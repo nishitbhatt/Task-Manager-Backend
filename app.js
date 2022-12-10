08b8ce185c029a656dd08fc3aeff729201012bad
nishitbhatt <nishit.bhatt96@gmail.com>
2022-12-10 16:31:48 +0530

    Change Headers

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(bodyParser.json());
 
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE');
     next();
 });
 
