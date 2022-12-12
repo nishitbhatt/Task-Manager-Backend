a5f7d30e71983656482985ff39de2775450fbffe
nishitbhatt <nishit.bhatt96@gmail.com>
2022-12-12 14:53:54 +0530

    Cors

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ import { default as UserTasksRoutes } from './routes/user-tasks.js';
 
 const app = express();
 
-const allowedOrigins = [
-    'capacitor://localhost',
-    'ionic://localhost',
-    'http://localhost',
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'http://localhost:8100',
-];
+// const allowedOrigins = [
+//     'capacitor://localhost',
+//     'ionic://localhost',
+//     'http://localhost',
+//     'http://localhost:3000',
+//     'http://localhost:8080',
+//     'http://localhost:8100',
+// ];
 
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Origin not allowed by CORS'));
-        }
-    },
-};
-app.options('*', cors(corsOptions));
-app.use(cors(corsOptions));
+// // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.includes(origin) || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Origin not allowed by CORS'));
+//         }
+//     },
+// };
+app.options('*', cors());
+app.use(cors());
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -55,7 +55,7 @@ app.use(bodyParser.json());
 // });
 
 // User Routes
-app.use('/api/user', cors(corsOptions), UserRoutes);
+app.use('/api/user', cors(), UserRoutes);
 // Task Routes
 app.use('/api/user-task', auth, UserTasksRoutes);
 
