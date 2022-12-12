fc4303ffcbd816ec696c067b2724dba69a14b003
nishitbhatt <nishit.bhatt96@gmail.com>
2022-12-12 14:15:07 +0530

    Cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const corsOptions = {
         }
     },
 };
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: false }));
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
